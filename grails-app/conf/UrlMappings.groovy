55a77a82e0369dfa4c55ef334c259507d9dc6b4d
djtfmartin <djtfmartin@gmail.com>
2012-12-04 06:35:10 +0000

    reordered
    git-svn-id: http://ala-sightings.googlecode.com/svn/trunk/fielddata@242 b53174df-531f-34c8-f11e-a8fbec9bff15

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -9,9 +9,9 @@ class UrlMappings {
         "/record/user/$userId"(controller: "record", action: "listForUser")
         "/record/sync/all"(controller: "record"){ action = [GET:"resyncAll"] }
         "/record/sync/$id"(controller: "record"){ action = [GET:"resyncRecord"] }
-        "/record/$id"(controller: "record"){ action = [GET:"getById", PUT:"updateById", DELETE:"deleteById", POST:"updateById"] }
         "/record/images"(controller: "record"){ action = [GET:"listRecordWithImages"] }
         "/record/images/"(controller: "record"){ action = [GET:"listRecordWithImages"] }
+        "/record/$id"(controller: "record"){ action = [GET:"getById", PUT:"updateById", DELETE:"deleteById", POST:"updateById"] }
         "/images"(controller: "record"){ action = [GET:"listRecordWithImages"] }
         "/images/"(controller: "record"){ action = [GET:"listRecordWithImages"] }
         "/images/update"(controller: "record"){ action = [POST:"updateImages"] }
